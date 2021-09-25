11dfbd5d1ce02886419636951ea21b0efcc23a53
Srinuvasarao Maripi <srinumaripi.nitsri@gmail.com>
2021-09-25 19:24:12 +0530

    login component

---
+++ src/Components/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>hello there</div>;
+  }
+}
+export default LoginPage;
